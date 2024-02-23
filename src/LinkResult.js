00a19b22210998adff8ed4db60cda6734b4fec3f
Akash Rai <akash03rai@gmail.com>
2024-02-24 01:23:58 +0530

    updated API

--- src/LinkResult.js
+++ src/LinkResult.js
@@ -7,15 +7,39 @@ const LinkResult = ({ inputValue }) => {
   const [copied, setCopied] = useState(false);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [trigger,setTrigger] = useState(true);
 
   const fetchData = async () => {
     try {
       setLoading(true);
       setError(false);
-      const res = await axios(`https://api.shrtco.de/v2/shorten?url=${inputValue}`);
-      setShortenLink(res.data.result.full_short_link);
+      
+      const options = {
+        method: "POST",
+        url: "https://url-shortener42.p.rapidapi.com/shorten/",
+        headers: {
+          "content-type": "application/json",
+          "X-RapidAPI-Key":
+            "0c4930f236msh09091024dc78c2ap1ad813jsn9da7c6fe8d63",
+          "X-RapidAPI-Host": "url-shortener42.p.rapidapi.com",
+        },
+        data: {
+          url: inputValue,
+          validity_duration: 10,
+        },
+      };
+
+      const response = await axios.request(options);
+      setShortenLink(response.data.url)
+      console.log(response.data);
+
     } catch(err) {
+      setShortenLink(null)
+      setTrigger(true);
       setError(err);
+      setTimeout(()=>{
+          setError("");
+      },3000)
     } finally {
       setLoading(false);
     }
@@ -39,25 +63,22 @@ const LinkResult = ({ inputValue }) => {
     return <p className="noData">Loading...</p>
   }
   if(error) {
-    return <p className="noData">Invalid Link (check for typos) :(</p>
+    return <p className="noData">Invalid Link (check for typos)</p>
   }
-
 
   return (
     <>
       {shortenLink && (
         <div className="result">
           <p>{shortenLink}</p>
-          <CopyToClipboard
-            text={shortenLink}
-            onCopy={() => setCopied(true)}
-          >
+          <CopyToClipboard text={shortenLink} onCopy={() => setCopied(true)}>
             <button className={copied ? "copied" : ""}>Copy link</button>
           </CopyToClipboard>
+ 
         </div>
       )}
     </>
-  )
+  );
 }
 
 export default LinkResult
